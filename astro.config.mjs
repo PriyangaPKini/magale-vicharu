540b3a376b0adab1fcf7fa787404415cba99bf98
Priyanga P Kini <priyangapkini99@gmail.com>
2026-05-06 14:35:51 +0530

    Update. site url to netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://priyangapkini.github.io',
+  site: 'https://magale-vicharu.netlify.app',
   output: 'static',
   integrations: [sitemap()],
 });
